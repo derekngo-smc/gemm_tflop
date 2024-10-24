4ea222fbee110677966ac19133a694bcbf960f84
derekngo-smc <derek.ngo@smc.co>
2024-10-24 08:48:54 +0800

    Create gemm_tflop.py

---
+++ gemm_tflop.py
@@ -0,0 +1,31 @@
+import torch
+import os
+import time
+
+
+def run_gemm_bf16():
+
+    device = torch.device(f'cuda:{0}')
+
+    M, N, K = 8192, 8192, 8192
+
+    A = torch.randn(M, K, device=device, dtype=torch.bfloat16)
+    B = torch.randn(K, N, device=device, dtype=torch.bfloat16)
+
+    torch.matmul(A, B)
+    torch.cuda.synchronize()
+
+    num_runs = 200000
+    start_time = time.perf_counter()
+    for _ in range(num_runs):
+        C = torch.matmul(A, B)
+    torch.cuda.synchronize()
+    end_time = time.perf_counter()
+
+    elapsed_time = end_time - start_time
+    flops = 2 * M * N * K * num_runs  # multiply-add is 2 operations
+    tflops = flops / (elapsed_time * 1e12)
+    print(f"Realized TFLOPS: {tflops:.2f}")
+
+if __name__ == "__main__":
+    run_gemm_bf16()
